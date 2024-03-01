61fe3e386499e96295876bb443c52841f37b0ac2
webmaster0109 <itdepartmentvfo@gmail.com>
2024-03-01 09:22:14 +0530

    app

--- sandcity_server/settings.py
+++ sandcity_server/settings.py
@@ -23,7 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rentalapp.apps.RentalappConfig',
+    'rentalapp',
 ]
 
 MIDDLEWARE = [
